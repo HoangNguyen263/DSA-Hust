794a949c88f75bdacc4f6b4987f2d81d02aede56
HoangNguyen263 <hoangdeltavn03@gmail.com>
2023-11-26 11:00:31 +0700

    update

--- Chapter2/TowerOfHanoi.c
+++ Chapter2/TowerOfHanoi.c
@@ -1,3 +1,14 @@
+/*Bài toán tháp Hà Nội
+• Có n đĩa với kích thước khác nhau và 3 cọc A, B, C
+• Ban đầu n đĩa nằm ở cọc A theo thứ tự đĩa nhỏ nằm
+trên và đĩa lớn nằm dưới
+• Tìm cách chuyển n đĩa này từ cọc A sang cọc C, sử
+dụng cọc B làm trung gian Lời giải theo nguyên tắc 
+    • Mỗi lần chỉ được chuyển 1 đĩa trên cùng từ 1 cọc sang 
+      cọc khác 
+    • Không được phép để xảy ra tình trạng đĩa to nằm bên
+      trên đĩa nhỏ
+*/
 #include<stdio.h>
 void TOH(int n, char A, char B, char C){
     if (n == 1){
